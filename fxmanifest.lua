a0861b5af974a8e5894ebffe155c80dd22ae5e96
BlackCodes <82057829+Muhammed121312@users.noreply.github.com>
2025-07-06 19:03:34 +0300

    Add fxmanifest.lua 1751817811961

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version 'cerulean'
+game 'gta5'
+
+name 'Multicharacter'
+description 'FiveM Multicharacter System'
+author 'BLACKBOXAI'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+    'client.lua'
+}
+
+files {
+    'html/index.html',
+    'html/style.css'
+}
